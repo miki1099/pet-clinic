33d0c67ed39ddf8ce84e4a0631d0e7d03eb581da
Michal G <miki.glodek@wp.pl>
2021-03-03 19:39:59 +0100

    Created repositories.

---
+++ pet-clinic-data/src/main/java/com/example/petclinic/repositories/PetTypeRepository.java
@@ -0,0 +1,8 @@
+package com.example.petclinic.repositories;
+
+import com.example.petclinic.model.PetType;
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface PetTypeRepository extends CrudRepository<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/example/petclinic/repositories/SpecialtyRepository.java
@@ -0,0 +1,8 @@
+package com.example.petclinic.repositories;
+
+import com.example.petclinic.model.Specialty;
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface SpecialtyRepository extends CrudRepository<Specialty, Long> {
+}
